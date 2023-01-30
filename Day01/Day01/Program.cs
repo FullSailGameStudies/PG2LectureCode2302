adafd652be18d79f71c17cbb1d4fa965b29ab033
Garrett Girod <ggirod@fullsail.edu>
2023-01-29 20:33:46 -0500

    Initial commit

---
+++ Day01/Day01/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day01
+{
+    /*                    METHODS          
+                                                               
+                ╔══════╗ ╔═══╗ ╔══════════╗ ╔════════════════╗ 
+                ║public║ ║int║ ║SomeMethod║ ║(int someParam) ║ 
+                ╚══════╝ ╚═══╝ ╚══════════╝ ╚════════════════╝ 
+                    │      │         │               │         
+              ┌─────┘      │         └──┐            └───┐     
+         ┌────▼────┐   ┌───▼───┐   ┌────▼───┐       ┌────▼────┐
+         │ Access  │   │ Return│   │ Method │       │Parameter│
+         │ modifier│   │ type  │   │  Name  │       │  list   │
+         └─────────┘   └───────┘   └────────┘       └─────────┘
+
+
+    
+    ╔═══════════╗ 
+    ║Return type║ Return type defines the type of the data being returned.
+    ╚═══════════╝
+    │
+    │
+    └── If NO data is returned, then use "void" for the return type.
+    │    public void PrintSomething()
+    │    {
+    │        Console.WriteLine("Something");
+    │    }
+    │
+    │
+    └── If a method returns data, then the return type must match the type of the data being returned.
+        public float GetMyGrade()
+        {
+            return 99.9F; //returning a float so set return type to float
+        }
+
+    ╔══════════╗ 
+    ║Parameters║ Parameters define the data passed to the method.
+    ╚══════════╝
+    │
+    │
+    └── If NO data is passed to the method, leave the parenthesis empty. EX: ( )
+    │    public void PrintSomething()
+    │   {  }
+    │
+    │
+    └── If passing data to the method, then define the variable the method will use to store the data.
+        Parameters are just variables therefore parameters need 2 things: <type> <name>
+        Example:
+        public void PrintMyGrade(float myGrade)
+        {
+            Console.WriteLine($"My grade is {myGrade}");
+        }
+
+        
+
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            /*
+              Calling a method
+                use the methods name.
+                1) if the method needs data (i.e. has parameters), you must pass data to the method that matches the parameter types
+                2) if the method returns data, it is usually best to store that data in a variable on the line where you call the method.
+             
+            */
+
+            /*
+                ╔══════════════════════════╗ 
+                ║Parameters: Pass by Value.║
+                ╚══════════════════════════╝ 
+             
+                Copies the value to a new variable in the method.
+                
+                For example, the AddOne method has a parameter called localNumber. localNumber is a variable that is local ONLY to the method.
+                The value of the variable in main, number, is COPIED to the variable in AddOne, localNumber.
+              
+            */
+            int number = 5;
+            int plusOne = AddOne(number);
+
+            /*
+                CHALLENGE 1:
+
+                    call the Sum method on the t1000 calculator. Print the sum that is returned.
+             
+            */
+            Calculator t1000 = new Calculator();
+
+
+
+
+            /*   
+                ╔═════════╗ 
+                ║ List<T> ║
+                ╚═════════╝ 
+                
+                [  Creating a List  ]
+                
+                List<T>  - the T is a placeholder for a type. It is a "Generic Type Parameter" to the class.
+                
+                When you want to create a List variable, replace T with whatever type of data you want to store in the List.
+            */
+            List<string> names = new List<string>(); //this list stores strings and only strings.
+
+            /*
+                CHALLENGE 2:
+
+                    Create a list that stores floats. Call the variable grades.
+             
+            */
+
+
+
+
+
+
+            /*   
+                ╔═════════╗ 
+                ║ List<T> ║
+                ╚═════════╝ 
+
+                [  Adding items to a List  ]
+
+                There are 2 ways to add items to a list:
+                1) on the initializer. 
+                2) using the Add method. 
+            */
+            List<char> letters = new List<char>() { 'B', 'a', 't', 'm', 'a', 'n' };
+            letters.Add('!');
+
+            /*
+                CHALLENGE 3:
+
+                    Add a few grades to the grades list you created in CHALLENGE 2.
+             
+            */
+
+
+
+
+
+
+            /*   
+                ╔═════════╗ 
+                ║ List<T> ║
+                ╚═════════╝ 
+
+                [  Looping over a List  ]
+
+                You can loop over a list with a for loop or a foreach loop.
+                1) for loop. use the Count property in the for condition.
+                2) foreach loop
+            */
+            for (int i = 0; i < letters.Count; i++)
+                Console.Write($" {letters[i]}");
+
+            foreach (var letter in letters)
+                Console.Write($" {letter}");
+
+            /*
+                CHALLENGE 4:
+
+                    1) Fix the Average method of the calculator class to calculate the average of the list parameter.
+                    2) Call the Average method on the t1000 variable and pass your grades list to the method.
+                    3) print the average that is returned.
+             
+            */
+
+
+            Console.ReadKey(true);
+        }
+
+        private static int AddOne(int localNumber)
+        {
+            return localNumber + 1;
+        }
+
+
+    }
+
+    class Calculator
+    {
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+
+        public float Average(List<float> numbers)
+        {
+            float avg = 0F;
+
+            //loop over the numbers and calculate the average
+
+            return avg;
+        }
+    }
+}
