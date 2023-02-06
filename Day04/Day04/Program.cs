80fb5dd8eaae4794230b302c930bbef14c993b44
Garrett Girod <girodg@hotmail.com>
2023-02-06 10:07:44 -0500

    Bats

--- Day04/Day04/Program.cs
+++ Day04/Day04/Program.cs
@@ -7,8 +7,18 @@ namespace Day04
 {
     internal class Program
     {
+        static void MyFirstRecursiveMethod(int N)
+        {
+            MyFirstRecursiveMethod(N + 1);
+        }
         static void Main(string[] args)
         {
+
+            ulong fac = Factorial(5);
+            Console.WriteLine($"5! = {fac}");
+            Console.ReadKey();
+
+            //MyFirstRecursiveMethod(0);
             //Recursion, Sorting, Searching
 
             /*
@@ -22,7 +32,7 @@ namespace Day04
               
             */
             int N = 0;
-            RecursiveLoop(N);
+            //RecursiveLoop(N);
             Console.ResetColor();
 
 
@@ -38,6 +48,14 @@ namespace Day04
                         Console.Write(' ');
                     }
             */
+            Bats(0);
+            Console.Write((char)66);
+            Console.Write((char)65);
+            Console.Write((char)84);
+            Console.Write((char)77);
+            Console.Write((char)65);
+            Console.Write((char)78);
+            Console.WriteLine("!!!!");
 
 
 
@@ -72,9 +90,21 @@ namespace Day04
 
         }
 
+        static void Bats(int i)
+        {
+            if (i < 100)
+            {
+                Console.Write((char)78);
+                Console.Write((char)65);
+                Console.Write(' ');
+                Bats(i+1);
+            }
+        }
+
 
         static void RecursiveLoop(int N)
         {
+            //return;//jumps to the last curly brace in the method
             //an Exit Condition. This will stop the loop when N >= Console.WindowWidth
             if (N < Console.WindowWidth)
             {
@@ -89,6 +119,22 @@ namespace Day04
                 Console.BackgroundColor = ConsoleColor.Blue;
                 Console.Write(' ');
             }
+        }//last line of code. method is done. returns.
+        static ulong Factorial(uint N)
+        {
+            ulong result = 1;
+            if (N > 1)
+            {
+                result = N * Factorial(N - 1);
+            }
+            return result;
+        }
+        static ulong Factorial2(uint N)
+        {
+            if (N <= 1) return 1;
+            ulong result = N * Factorial2(N - 1);
+            return result;
         }
     }
+
 }
