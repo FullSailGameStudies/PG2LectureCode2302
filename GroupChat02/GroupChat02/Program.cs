25756632d8c3d566b66fe0f663e3d27c24d77fac
Garrett Girod <girodg@hotmail.com>
2023-02-08 20:47:35 -0500

    group chat 02

---
+++ GroupChat02/GroupChat02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello Gotham!";
+            Console.WriteLine(hello[0]);
+
+            Console.WriteLine(hello.Substring(0,1));
+        }
+    }
+}
